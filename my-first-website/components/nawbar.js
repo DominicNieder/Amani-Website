de705742e89dabc82379fd7f67ce5bbcecfb8d34
DominiNieder <dominic.nieder@gmail.com>
2024-06-20 15:47:41 +0200

    Clean new project

---
+++ my-first-website/components/nawbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function nawbar() {
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default nawbar
